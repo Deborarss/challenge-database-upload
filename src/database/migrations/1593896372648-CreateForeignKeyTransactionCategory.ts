b39369f4173edf59bb94ea392018c185a0f8e7ec
Debora Souza <debora_rss@hotmail.com>
2020-07-04 18:11:39 -0300

    Create categories, transactions and TransactionCategory foreign key migrations

---
+++ src/database/migrations/1593896372648-CreateForeignKeyTransactionCategory.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class CreateForeignKeyTransactionCategory1593896372648
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategory',
+        columnNames: ['category_id'],
+        referencedTableName: 'categories',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+  }
+}
